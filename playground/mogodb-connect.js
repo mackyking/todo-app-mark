1647dea971c4dc27c6aea8e3067a1f7a24b09d55
mackyking <marinnehterg@gmail.com>
2018-06-09 03:18:30 -0700

    Set up app for heroku

--- playground/mogodb-connect.js
+++ playground/mogodb-connect.js
@@ -1,4 +1,10 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');// Using ES6 destructuring technique.
+
+// ES6 coding, object destructuring.
+
+
+
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     if (err) {
@@ -7,7 +13,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp')
 
-    db.collection('Todos').imsertOne({
+    /*
+    db.collection('Todos').insertOne({
+        _id: 123,
         text: 'Something to do',
         Completed: false
     }, (err, result) => {
@@ -17,7 +25,28 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 
         console.log(JSON.stringify(result.ops, undefined, 2));
     });
+   
+    // New group codes
+     
+   db.collection('Users').insertOne({
+    name: 'Mark',
+    location: 'Palawan'
+}, (err, result) => {
+    if (err) {
+        return console.log('Unable to insert user', err);
+    }
 
+    console.log(JSON.stringify(result.ops, undefined, 2));
+});
+   
+// New group codes, Object destructuring of ES6 syntax
+var user = {name: 'Mark', age: 32};
+var {name} = user;
+console.log(name);
+
+    */
+
+   
     client.close();
 });
 
